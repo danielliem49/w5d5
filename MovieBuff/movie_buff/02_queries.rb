f1256084bae562e2ad97eaeaaf9bd283795a5abc
Daniel Liem <danielliem49@gmail.com>
2023-01-07 12:56:03 -0800

    MovieBuff and Polls

---
+++ MovieBuff/movie_buff/02_queries.rb
@@ -0,0 +1,58 @@
+def eighties_b_movies
+  # List all the movies from 1980-1989 with scores falling between 3 and 5
+  # (inclusive). Show the id, title, year, and score.
+  Movie.select(:id,:title,:yr,:score)
+    .where(yr: 1980..1989)
+    .where(score: 3..5)
+  
+end
+
+def bad_years
+  # List the years in which no movie with a rating above 8 was released.
+  Movie
+    .where('yr NOT IN (SELECT yr FROM movies WHERE movies.score > 8)')
+    .distinct
+    .pluck(:yr)
+    
+
+    # SELECT yr
+    # FROM movies
+    # WHERE yr NOT IN (
+    #   SELECT yr
+    #   FROM movies
+    #   WHERE movies.score > 8
+    # )
+end
+
+def cast_list(title)
+  # List all the actors for a particular movie, given the title.
+  # Sort the results by starring order (ord). Show the actor id and name.
+  Actor
+    .joins(:movies)
+    .where(movies: {title: title})
+    .order(:ord)
+    .select(:id, :name)
+end
+
+def vanity_projects
+  # List the title of all movies in which the director also appeared as the
+  # starring actor. Show the movie id, title, and director's name.
+  #director_id: :actor_id
+  # Note: Directors appear in the 'actors' table.
+  Movie
+    .select(:id,:title,:name)
+    .joins(:actors)
+    .where('movies.director_id = actors.id AND castings.ord = 1') #director_id: :actor_id
+end
+
+def most_supportive
+  # Find the two actors with the largest number of non-starring roles.
+  # Show each actor's id, name, and number of supporting roles.
+  Actor
+    .select('actors.id, actors.name, COUNT(*) as roles')
+    .joins(:movies)
+    .where('castings.ord != 1')
+    .group(:id)
+    .order('roles DESC')
+    .limit(2)
+end
